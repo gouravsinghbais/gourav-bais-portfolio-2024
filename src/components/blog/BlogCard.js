dcc0dceb7bbc2a0e563a45b93db77350155e28ff
lokesh1pawar <72260908+lokesh1pawar@users.noreply.github.com>
2024-08-26 17:12:04 +0530

    updated blog

--- src/components/blog/BlogCard.js
+++ src/components/blog/BlogCard.js
@@ -5,7 +5,7 @@ const BlogCard = ({ blog }) => {
   return (
     <Link to={`/blog/${blog.id}`} className="card bg-bgMain rounded-lg overflow-hidden m-4 cursor-pointer font-custom1 font-custom2 leading-8 p-5 w-[360px] h-[416px] shadow-black shadow-sm border-for-box">
       <div className='overflow-hidden'>
-      <img src={blog.image} alt={blog.title} className="w-full h-48 object-cover transition-transform transform hover:scale-110 duration-500 rounded-lg" />
+      <img src={`${process.env.PUBLIC_URL}/${blog.image}`} alt={blog.title} className="w-full h-48 object-cover transition-transform transform hover:scale-110 duration-500 rounded-lg" />
       </div>
       <div className="p-4 mt-4">
         <h2 className="text-navFontColor font-semibold text-2xl hover:text-redForHoverNav duration-300">{blog.title}</h2>
